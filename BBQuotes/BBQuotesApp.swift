de16daf15328f526fd409e4d56ee98b0f6bdda35
pallavijoshi <pallavijoshi4@gmail.com>
2024-09-17 20:13:26 +0200

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Pallavi Joshi on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBQuotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BBQuotes
+//
+//  Created by Pallavi Joshi on 17/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
